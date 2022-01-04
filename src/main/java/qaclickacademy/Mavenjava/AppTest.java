357cd325f692c80732d308a84d46bc244c3925b0
arindam <darindam240@gmail.com>
2022-01-04 12:25:27 +0530

    first commit

---
+++ src/main/java/qaclickacademy/Mavenjava/AppTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+
+	@Test
+	public void NativAppAndroid()
+	
+	{
+		System.out.println("NativeApp");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");	
+	}
+
+}
---
+++ src/main/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//testng xml file from
+	
+	@Test
+	public void BrowserAutomation()
+	
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+
+		System.out.println("elementsUi");
+	}
+	
+	
+
+}
